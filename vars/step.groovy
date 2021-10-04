9d27f2e860f6f73d1b730c4656249f570a2b22f1
annemacalalad <91460116+annemacalalad@users.noreply.github.com>
2021-10-05 00:08:12 +0800

    Update step.groovy

--- vars/step.groovy
+++ vars/step.groovy
@@ -6,6 +6,10 @@ def buildImage(){
   myapp = docker.build("annemacalalad/ledger-service:${env.BUILD_ID}", "--build-arg VERSION=${env.BUILD_ID} .")
 }
 
+def npmBuild() {
+  myapp = docker.build("annemacalalad/nodejs-test:${env.BUILD_ID}")
+}
+
 def pushImage(){
   docker.withRegistry('https://registry.hub.docker.com', 'dockerhub'){
     myapp.push("latest")
