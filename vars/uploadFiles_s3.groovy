04cd94bc43bc98b076bc47e6dc8a812cad8549f7
annemacalalad <91460116+annemacalalad@users.noreply.github.com>
2021-10-12 15:36:15 +0800

    Update uploadFiles_s3.groovy

--- vars/uploadFiles_s3.groovy
+++ vars/uploadFiles_s3.groovy
@@ -1,4 +1,4 @@
-def call(Map stageParams = [awsRegion: "us-east-1", s3Bucket: "test-bucket-10082021", s3Path: "/"]) {
+def call(Map stageParams = [awsRegion: "us-east-1", s3Bucket: "testbucket-anne", s3Path: "/"]) {
     withAWS(region: stageParams.awsRegion, credentials:"${AWS_CRED}") {
         awsIdentity()
         s3Upload( 
